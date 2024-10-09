53036a61c98a296594566f63e63763c0ea920cc9
Alena Timofeeva <alena@dimas-imac.lan>
2024-10-09 06:11:20 -0700

    Add toggle to switch to the mesh object representation

--- 02/main.js
+++ 02/main.js
@@ -19,12 +19,11 @@ scene = new THREE.Scene();
 // Add a simple geometry and material
 const geometry = new THREE.BoxGeometry();
 const material = new THREE.MeshBasicMaterial({ 
-    color: 0x00ff00,
+    color: 0x80b5a3,
     transparent: true,  // Enable transparency
-    opacity: 0.5        // Set opacity (0 = fully transparent, 1 = fully opaque)
+    opacity: 0.7        // Set opacity (0 = fully transparent, 1 = fully opaque)
     });
-var wireframeMaterial = new THREE.MeshBasicMaterial({ wireframe: true });
-const cube = new THREE.Mesh(geometry, wireframeMaterial);
+const cube = new THREE.Mesh(geometry, material);
 
 scene.add(cube);
 
@@ -69,6 +68,19 @@ scaleFolder.add(cube.scale, 'y', 0, 3);
 scaleFolder.add(cube.scale, 'z', 0, 3);
 scaleFolder.open();
 
+// Add toggle for the switcing between mesh and material
+const debugParams = {
+    showWireframe: false
+  };
+  
+  gui.add(debugParams, 'showWireframe').name('Show Wireframe').onChange(() => {
+    // Toggle wireframe on/off for all objects in the scene
+    scene.traverse((child) => {
+      if (child.isMesh) {
+        child.material.wireframe = debugParams.showWireframe;
+      }
+    });
+  });
 
 document.body.appendChild(renderer.domElement);
 
