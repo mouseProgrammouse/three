a0c16f3c97ce45801be05436248ff8a1bd15ec5c
Alena Timofeeva <alena@dimas-imac.lan>
2024-10-10 16:24:21 -0700

    Add logic to render N amount of stairs

--- 03/main.js
+++ 03/main.js
@@ -22,7 +22,7 @@ const stairHeight = 1,
   stairWidth = 4,
   stairDepth = 0.2;
 
-const createStairSegment = (scene) => {
+const createStairSegment = (scene, x, y, z) => {
   // Add a simple geometry and material
   const topOfStairGeometry = new THREE.BoxGeometry(
     stairWidth,
@@ -35,39 +35,62 @@ const createStairSegment = (scene) => {
     stairDepth
   );
 
-  const material = new THREE.MeshBasicMaterial({
-    color: MAIN_COLOR,
-    transparent: true, // Enable transparency
-    opacity: 0.7, // Set opacity (0 = fully transparent, 1 = fully opaque)
-  });
   var wireframeMaterial = new THREE.MeshBasicMaterial({ wireframe: true });
 
   const topOfStair = new THREE.Mesh(topOfStairGeometry, wireframeMaterial);
-  const frontOfStair = new THREE.Mesh(frontOfStairGeometry, material);
+  const frontOfStair = new THREE.Mesh(frontOfStairGeometry, wireframeMaterial);
 
-  topOfStair.position.y = stairHeight + stairDepth / 2;
-  topOfStair.position.z = stairWidth / 2 - stairDepth / 2;
+  topOfStair.position.y = y + stairHeight + stairDepth / 2;
+  topOfStair.position.z = z + stairHeight / 2;
 
-  frontOfStair.position.y = stairHeight / 2;
+  frontOfStair.position.y = y + stairHeight / 2;
+  frontOfStair.position.z = z + stairDepth / 2;
 
   scene.add(topOfStair);
   scene.add(frontOfStair);
 };
-// Set the size of the renderer to match the window size
-renderer.setSize(width, height);
-createStairSegment(scene);
 
-addGridAndAxesHelpers(scene, camera, THREE);
-document.body.appendChild(renderer.domElement);
-
-// Create the OrbitControls
-const controls = new OrbitControls(camera, renderer.domElement);
-controls.target.set(0, 0, 0); // Set the target of the controls to the center of the cube
-controls.update();
+const addOrbitControlls = () => {
+  // Create the OrbitControls
+  const controls = new OrbitControls(camera, renderer.domElement);
+  controls.target.set(0, 0, 0);
+  controls.update();
+};
 
 const animate = () => {
   requestAnimationFrame(animate);
   renderer.render(scene, camera);
 };
+
+const initForm = () => {
+  document.getElementById("submitStairsAmount").addEventListener("click", (event) => {
+  // Prevent the default form submission behavior
+  event.preventDefault();
+
+  // Get the number of sides from the input field
+  const amountOfStairs = document.getElementById("stairsAmount").value;
+  clearScene();
+  addGridAndAxesHelpers(scene, camera, THREE);
+  for (let i = 0; i < amountOfStairs; i++) {
+    createStairSegment(scene, 0, stairHeight * i, stairHeight * i);
+  }
+  addOrbitControlls();
+  })
+}
+
+  // Function to clear the scene
+const clearScene = () => {
+  while (scene.children.length > 0) {
+      scene.remove(scene.children[0]);
+  }
+}
+
+
+// Set the size of the renderer to match the window size
+renderer.setSize(width, height);
+document.body.appendChild(renderer.domElement);
+addGridAndAxesHelpers(scene, camera, THREE);
+addOrbitControlls();
+initForm();
 
 animate();
